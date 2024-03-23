c2f98f456d122f600e6a9e068d3aeac677c2831d
Parniyan Habibi <p.habibi1997@gmail.com>
2024-03-23 15:33:48 +0330

    add new project about bouncing balls

--- src/014-Bouncing-Balls/script.js
+++ src/014-Bouncing-Balls/script.js
@@ -4,37 +4,45 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let x = Math.floor(Math.random() * canvas.width);
-let y = Math.floor(Math.random() * canvas.width);
 let radius = 25;
 let xSpeed = 5;
 let ySpeed = 10;
 
-function balls() {
-    for (let i = 0; i < 20; i++) {
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2 * Math.PI);
-        ctx.closePath();
-        ctx.fillStyle = 'white';
-        ctx.fill();
-    }
+let ball = [];
+
+for (let i = 0; i < 20; i++) {
+    let properties = {
+        x: Math.floor(Math.random() * canvas.width),
+        y: Math.floor(Math.random() * canvas.width),
+    };
+
+    ball.push(properties);
 }
 
 function bouncing() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    balls();
+    for (let item of ball) {
+        let x = item.x;
+        let y = item.y;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fillStyle = 'white';
+        ctx.fill();
 
-    x = x + xSpeed;
-    y = y + ySpeed;
+        x = x + xSpeed;
+        y = y + ySpeed;
 
-    if (x + radius >= canvas.width || x - radius <= 0) {
-        xSpeed = -xSpeed;
-    }
+        if (x + radius >= canvas.width || x - radius <= 0) {
+            xSpeed = -xSpeed;
+        }
 
-    if (y + radius >= canvas.height || y - radius <= 0) {
-        ySpeed = -ySpeed;
+        if (y + radius >= canvas.height || y - radius <= 0) {
+            ySpeed = -ySpeed;
+        }
+
     }
 }
 
