2d2a91f7202b9b56ddfe3e4be232f0c756f8d817
Parniyan Habibi <p.habibi1997@gmail.com>
2024-03-27 14:41:08 +0330

    constellation project

--- src/014-Bouncing-Balls/script.js
+++ src/014-Bouncing-Balls/script.js
@@ -9,7 +9,7 @@ let height = [-10, 10];
 
 let balls = [];
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 800; i++) {
     let properties = {
         x: Math.floor(Math.random() * canvas.width),
         y: Math.floor(Math.random() * canvas.width),
@@ -20,10 +20,14 @@ for (let i = 0; i < 500; i++) {
     balls.push(properties);
 }
 
+function mouseMove(event) {
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
 
-function bouncing() {
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let mouseAreaXRight = mouseX + 300;
+    let mouseAreaYBottom = mouseY + 300;
+    let mouseAreaLeft = mouseX - 300;
+    let mouseAreaYTop = mouseY - 300;
 
     for (let ball of balls) {
         for (let otherBalls of balls) {
@@ -31,20 +35,28 @@ function bouncing() {
                 let xSpace = ball.x - otherBalls.x;
                 let ySpace = ball.y - otherBalls.y;
 
-                let space = Math.sqrt(xSpace*xSpace + ySpace*ySpace);
-
-                if (space <= 100) {
-                    ctx.beginPath();
-                    ctx.moveTo(ball.x, ball.y);
-                    ctx.lineTo(otherBalls.x, otherBalls.y);
-                    ctx.strokeStyle = 'white';
-                    ctx.lineWidth = '.3';
-                    ctx.stroke();
-                    ctx.closePath();
+                if (ball.x <= mouseAreaXRight && ball.y <= mouseAreaYBottom && ball.x >= mouseAreaLeft && ball.y >= mouseAreaYTop) {
+                    let space = Math.sqrt(xSpace * xSpace + ySpace * ySpace);
+                    if (space <= 100) {
+                        ctx.beginPath();
+                        ctx.moveTo(ball.x, ball.y);
+                        ctx.lineTo(otherBalls.x, otherBalls.y);
+                        ctx.strokeStyle = 'white';
+                        ctx.lineWidth = '.1';
+                        ctx.stroke();
+                        ctx.closePath();
+                    }
                 }
             }
         }
+    }
+}
 
+function bouncing() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let ball of balls) {
         ctx.beginPath();
         ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
         ctx.closePath();
@@ -62,11 +74,10 @@ function bouncing() {
         if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
             ball.ySpeed = -ball.ySpeed;
         }
-
     }
 }
 
 setInterval(bouncing, 80);
 
-
+document.addEventListener('mousemove', mouseMove);
 
